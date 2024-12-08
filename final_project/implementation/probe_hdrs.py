20ed443a6bbac81ca6eabe25d41812bc573d8299
fffuuuming <benson.oor@gmail.com>
2024-12-08 11:53:41 +0800

    modify probe_hdrs.py for custom header

--- final_project/implementation/probe_hdrs.py
+++ final_project/implementation/probe_hdrs.py
@@ -2,24 +2,25 @@ from scapy.all import *
 
 TYPE_PROBE = 0x812
 
-class Probe(Packet):
-   fields_desc = [ ByteField("hop_cnt", 0)]
+class RoutingLabel(Packet):
+   fields_desc = [ BitField("egress_spec", 0, 7),
+                   BitField("bos", 0, 1)]
+
+class Counter(Packet):
+   fields_desc = [ ByteField("visited_count", 0, 7),]
 
 class ProbeData(Packet):
    fields_desc = [ BitField("bos", 0, 1),
-                   BitField("swid", 0, 7),
-                   ByteField("port", 0),
-                   IntField("byte_cnt", 0),
-                   BitField("last_time", 0, 48),
-                   BitField("cur_time", 0, 48)]
+                   BitField("switch_id", 0, 7),
+                   ByteField("rule_id", 0),
+                   BitField("in_port", 0, 7),
+                   BitField("out_port", 0, 7)]
+   
 
-class ProbeFwd(Packet):
-   fields_desc = [ ByteField("egress_spec", 0)]
-
-bind_layers(Ether, Probe, type=TYPE_PROBE)
-bind_layers(Probe, ProbeFwd, hop_cnt=0)
-bind_layers(Probe, ProbeData)
+bind_layers(Ether, RoutingLabel, type=TYPE_PROBE)
+bind_layers(RoutingLabel, RoutingLabel, bos=0)
+bind_layers(RoutingLabel, Counter, bos=1)
+bind_layers(Counter, IP, visited_count=0)
+bind_layers(Counter, ProbeData)
 bind_layers(ProbeData, ProbeData, bos=0)
-bind_layers(ProbeData, ProbeFwd, bos=1)
-bind_layers(ProbeFwd, ProbeFwd)
-
+bind_layers(ProbeData, IP, bos=1)
