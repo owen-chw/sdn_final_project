8db1e39a315f178532344c004aa47bce1cdf5040
fffuuuming <benson.oor@gmail.com>
2024-12-08 14:52:48 +0800

    modify send.py and some erros in probe_hdrs.py

--- final_project/implementation/probe_hdrs.py
+++ final_project/implementation/probe_hdrs.py
@@ -3,18 +3,18 @@ from scapy.all import *
 TYPE_PROBE = 0x812
 
 class RoutingLabel(Packet):
-   fields_desc = [ BitField("egress_spec", 0, 7),
+   fields_desc = [ BitField("egress_spec", 0, 9),
                    BitField("bos", 0, 1)]
 
 class Counter(Packet):
-   fields_desc = [ ByteField("visited_count", 0, 7),]
+   fields_desc = [ ByteField("visited_count", 0)]
 
 class ProbeData(Packet):
    fields_desc = [ BitField("bos", 0, 1),
-                   BitField("switch_id", 0, 7),
-                   ByteField("rule_id", 0),
-                   BitField("in_port", 0, 7),
-                   BitField("out_port", 0, 7)]
+                   ByteField("switch_id", 0),
+                   ShortField("rule_id", 0),
+                   BitField("in_port", 0, 9),
+                   BitField("out_port", 0, 9)]
    
 
 bind_layers(Ether, RoutingLabel, type=TYPE_PROBE)
--- final_project/implementation/send.py
+++ final_project/implementation/send.py
@@ -1,30 +1,60 @@
 #!/usr/bin/env python3
+import socket
 import sys
-import time
+
+from scapy.all import *
 
 from probe_hdrs import *
 
 
+def get_if():
+    ifs=get_if_list()
+    iface=None # "h1-eth0"
+    for i in get_if_list():
+        if "eth0" in i:
+            iface=i
+            break;
+    if not iface:
+        print("Cannot find eth0 interface")
+        exit(1)
+    return iface
+
 def main():
 
-    probe_pkt = Ether(dst='ff:ff:ff:ff:ff:ff', src=get_if_hwaddr('eth0')) / \
-                Probe(hop_cnt=0) / \
-                ProbeFwd(egress_spec=4) / \
-                ProbeFwd(egress_spec=1) / \
-                ProbeFwd(egress_spec=4) / \
-                ProbeFwd(egress_spec=1) / \
-                ProbeFwd(egress_spec=3) / \
-                ProbeFwd(egress_spec=2) / \
-                ProbeFwd(egress_spec=3) / \
-                ProbeFwd(egress_spec=2) / \
-                ProbeFwd(egress_spec=1)
+    if len(sys.argv)<2:
+        print('pass 2 arguments: <destination>')
+        exit(1)
+
+    addr = socket.gethostbyname(sys.argv[1])
+    iface = get_if()
+    print("sending on interface %s to %s" % (iface, str(addr)))
 
     while True:
-        try:
-            sendp(probe_pkt, iface='eth0')
-            time.sleep(1)
-        except KeyboardInterrupt:
-            sys.exit()
+        print()
+        s = str(input('Type space separated port nums '
+                          '(example: "2 3 2 2 1") or "q" to quit: '))
+        if s == "q":
+            break;
+        print()
+
+        i = 0
+        pkt =  Ether(src=get_if_hwaddr(iface), dst='ff:ff:ff:ff:ff:ff');
+        for p in s.split(" "):
+            try:
+                pkt = pkt / RoutingLabel(bos=0, egress_spec=int(p))
+                i = i+1
+            except ValueError:
+                pass
+        if pkt.haslayer(RoutingLabel):
+            pkt.getlayer(RoutingLabel, i).bos = 1
+
+        pkt = pkt / Counter(visited_count=0) / IP(dst=addr) / UDP(dport=4321, sport=1234)
+        pkt.show2()
+        sendp(pkt, iface=iface, verbose=False)
+
+    #pkt = pkt / SourceRoute(bos=0, port=2) / SourceRoute(bos=0, port=3);
+    #pkt = pkt / SourceRoute(bos=0, port=2) / SourceRoute(bos=0, port=2);
+    #pkt = pkt / SourceRoute(bos=1, port=1)
 
 if __name__ == '__main__':
     main()
